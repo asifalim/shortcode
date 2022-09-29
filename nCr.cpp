3c57951f62bc396adf03049e2d2e5554c84e7135
Alim Uddin Asif <66546728+asifalim@users.noreply.github.com>
2022-09-29 23:55:13 +0600

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
-const int mxN=1e6+4,mod=1e9+7;
-ll f1[mxN],f2[mxN],inv[mxN];
+const int mxn=1e6+4,mod=1e9+7;
+ll f1[mxn],f2[mxn],inv[mxn];
 void inverse()
 {
     inv[1]=1;
-    for(int i=2;i<mxN;i++)inv[i]=mod-mod/i*inv[mod%i]%mod;
+    for(int i=2;i<mxn;i++)inv[i]=mod-mod/i*inv[mod%i]%mod;
     f1[0]=f2[0]=1;
-    for(int i=1;i<mxN;i++)f1[i]=(f1[i-1]*i)%mod,f2[i]=(f2[i-1]*inv[i])%mod;
+    for(int i=1;i<mxn;i++)f1[i]=(f1[i-1]*i)%mod,f2[i]=(f2[i-1]*inv[i])%mod;
 }
 ll ncr(int n,int r)
 {
