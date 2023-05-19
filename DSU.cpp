aed3c97aadc75d1eed935d1c604ec3cfac79bcf8
Alim Uddin Asif <66546728+asifalim@users.noreply.github.com>
2023-05-20 02:19:52 +0600

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,38 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define ll long long int
-#define dbg(a,b,c) cerr<<a<<"  "<<b<<"  "<<c<<endl;
-#define mem(a,b) memset(a,b,sizeof(a))
-#define endl "\n"
-#define INF 1e18
-#define w(t) cin>>t;while(t--)
+#define dbg(a,b,c,d) cerr<<a<<"  "<<b<<"  "<<c<<"  "<<d<<endl;
 #define kill(a) {cout<<a<<endl;continue;}
+#define KILL(a) {cout<<a<<endl;return 0;}
+#define debug cerr<<"Error Found"<<endl;
+#define mem(a,b) memset(a,b,sizeof(a))
+#define lcm(a, b) (a/__gcd(a,b))*b
+#define w(t) cin>>t;while(t--)
 #define pi  2 * acos(0.0)
-int t,ans=0,tot=0,kk=0;
-const int mxn=1e5+3,mod=1e9+7;
-int par[mxn];
-int find(int a)
+#define ll long long int
+#define endl "\n"
+#define INF (1LL<<61)
+int t,cs=0;
+const int mxn=1e6+2,mod=1e9+7;
+int par[mxn],rnk[mxn];
+void refresh()
 {
-    return (par[a]<0?a:find(par[a]));
+    for(int i=1;i<=mxn;i++)par[i]=i,rnk[i]=1;
+}
+int find(int n)
+{
+    return par[n]==n?n:find(par[n]);
 }
 void merge(int a,int b)
 {
-    if((a=find(a))==(b=find(b)))return;
-    if(par[a]>par[b])swap(a,b);
-    par[a]+=par[b];
+    a=find(a),b=find(b);
+    /*if(a==b)return;
+    if(rnk[a]<rnk[b])swap(a,b);
+    rnk[a]+=rnk[b];*/
     par[b]=a;
 }
-signed main()
+int32_t main()
 {
+
     //fast;
-   // w(t)
-    //{
-        ll n,m,a,b,c,d,e,i,j,k,sm=0,sm1=0,cn=0,cn1=0,mx=-1e9,mn=1e9;
-        string s,ss,sr;
-        bool f=true;
-        cin>>n>>m;fill(par,par+n+1,-1);
-        for(i=1;i<=m;i++)cin>>a>>b,merge(a,b);
-        for(i=1;i<=n;i++)cout<<par[i]<<" ";cout<<endl;
-    //}
+    w(t)
+    {
+        int n;
+        cin>>n;
+        refresh();
+        int a,b;
+        for(int i=1;i<=n;i++)cin>>a>>b,merge(a,b);
+
+    }
 }
